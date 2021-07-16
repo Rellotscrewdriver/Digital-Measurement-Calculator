8f12ea89736cd118795e27b7cc8aa720f5a862ec
Rellotscrewdriver <JKanimations71@gmail.com>
2021-07-16 08:16:00 +0530

    Fixed the issue when symbol is entered and stored

--- exception_handling.cpp
+++ exception_handling.cpp
@@ -1,25 +1,29 @@
 #include "exception_handling.h"
 #include "declarations.h"
-#include <limits>
+#include "2D_shapes/Shape_2D.h"
 
-void Exception_Handling::check_if_typed_string(){
-    if(cin.fail()){
-        cout << "\nYou typed a string, try again" << endl << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cin.sync();//discards all that is left in buffer
-        cin >> ws; //discards all the whitespace before any operation
+void Exception_Handling::check_if_typed_string() {
+	if (cin.fail()) {
+		cout << invaild_message_string << endl;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		/*
+            cin.sync()
+            cin >> ws;
+
+            these two syntaxes were removed due to their inconsitency
+            with forward and backslash symbols
+		*/
     }
+    Shape_2D();
 }
 
-void Exception_Handling::check_if_typed_string_and_invaild_number(){
-    if(cin.fail()){
-        cout << "\nYou typed a string, try again" << endl << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cin.sync();
-        cin >> ws;
-    } else {
-        cout << "\nInvaild Input Detected" << endl << endl;
-    }
+void Exception_Handling::check_if_typed_string_and_invaild_number() {
+	if (cin.fail()) {
+		cout << invaild_message_string << endl;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	} else
+		cout << invaild_message << endl;
+
 }
--- exception_handling.h
+++ exception_handling.h
@@ -3,10 +3,14 @@
 #include "declarations.h"
 
 class Exception_Handling : public declarations {
-    public:
-        void check_if_typed_string();
-        void check_if_typed_string_and_invaild_number();
-        string error_message = "Inputs Can't be Negative or Zero\n\n";
+public:
+	void check_if_typed_string();
+	void check_if_typed_string_and_invaild_number();
+	string error_message = "Inputs Can't be Negative or Zero\n\n";
+
+private:
+	string invaild_message_string = "You typed a string or an invaild character, try again";
+	string invaild_message = "\nInvaild Input Detected, try again";
 };
 
 #endif
