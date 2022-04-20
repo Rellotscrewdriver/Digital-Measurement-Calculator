5da09191b65474997cff8af469bd179daf2ec896
Rellotscrewdriver <JKanimations71@gmail.com>
2022-04-20 14:27:15 +0530

    Changed the UI

--- Display/Console_Lines.cpp
+++ Display/Console_Lines.cpp
@@ -1,44 +1,43 @@
 #include "display.h"
 
 void Display::list_2D_Shapes_text(){
-    cout << "Enter any 2D shape respectively\n"
-		 << "1. Square \n2. Rectangle \n3. Parallelogram \n4. Circle \n5. Quadrilateral \n6. Trapezium \n7. Polygon \n"
-	     << "8" << switch_to_3D
-		 << "9" << exit_str << endl;
+    cout << "\nEnter any 2D shape respectively\n"
+		 << "[1] Square \t  [2] Rectangle \t[3] Parallelogram \n[4] Circle \t  [5] Quadrilateral \t[6] Trapezium \n[7] Polygon \t"
+	     << "  [8]" << switch_to_3D
+		 << "[9]" << exit_str;
 }
 
 void Display::list_3D_Shapes_text(){
-	cout << "Enter any 3D shape respectively\n"
-		 << "1. Cube \n2. Cuboid \n3. Sphere \n4. Hemisphere \n5. Cone \n6. Cylinder \n"
-	     << "7" << switch_to_2D
-         << "8" << exit_str << endl;
+	cout << "\nEnter any 3D shape respectively\n"
+		 << "[1] Cube \t  [2] Cuboid \n[3] Sphere \t  [4] Hemisphere \n[5] Cone \t  [6] Cylinder \n"
+	     << "[7]" << switch_to_2D
+         << "  [8]" << exit_str;
 }
 
 void Display::measurement_type_2D_text(){
     cout << str_measure_type
-		 << "1. Area\n"
-		 << "2. perimeter\n"
-		 << "3" << previous_step
-		 << "4" << exit_str
+		 << "[1] Area\t"
+		 << "  [2] perimeter\n"
+		 << "[3]" << previous_step
+		 << "[4]" << exit_str
 		 << input;
 }
 
 void Display::measurement_type1_3D_text(){
 	cout << str_measure_type
-	     << "1. Surface Area"
-	     << "2. Volume"
-	     << "3" << previous_step
-	     << "4" << exit_str
+	     << "[1] Surface Area  "
+	     << "[2] Volume\n"
+	     << "[3]" << previous_step
+	     << "[4]" << exit_str
 	     << input;
 }
 
 void Display::measurement_type2_3D_text(){
 	cout << str_measure_type
-		 << "1. Curved Surface Area\n"
-		 << "2. Total Surface Area\n"
-		 << "3. Volume\n"
-		 << "4" << previous_step
-		 << "5" << exit_str
+		 << "[1] Curved Surface Area\t"
+		 << "[2] Total Surface Area\n"
+		 << "[3] Volume  "
+		 << "[4]" << previous_step
+		 << "[5]" << exit_str
 		 << input;
 }
-
--- Display/Display.h
+++ Display/Display.h
@@ -5,18 +5,17 @@
 
 class Display {
 public:
-
-	declarations var;
 	string Shape_Name;
+    string dimensional;
 
-	const string exit_str = ". exit the program\n";
-    const string switch_to_3D = ". switch to 3D\n";
-    const string switch_to_2D = ". Switch to 2D\n";
+	const string exit_str = " exit the program\n";
+    const string switch_to_3D = " switch to 3D\t";
+    const string switch_to_2D = " Switch to 2D";
 	const string input = "Input: ";
 	const string output = "Output: ";
 	const string console_message = "Press any key to continue...";
-	const string str_measure_type = "Enter the measure type\n";
-    const string previous_step = ". Previous step\n";
+	const string str_measure_type = "\nEnter the measure type\n";
+    const string previous_step = " Previous step ";
 
 	bool dimension_compare2D(string parameter);
     bool dimension_compare3D(string parameter);
@@ -31,8 +30,9 @@ public:
 	void measurement_type1_3D_text();
 	void measurement_type2_3D_text();
 
-    void one_parameter_text();
-    void two_parameters_text();
+    string one_parameter_text(int ShapeNum);
+    string two_parameters_text(int ShapeNum, int input_step);
+    string three_parameters_text(int ShapeNum);
 
 private:
 
