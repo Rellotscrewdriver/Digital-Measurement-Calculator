fe0df5883eac5acd4190ba290ef156a52cb4c4b2
Rellotscrewdriver <JKanimations71@gmail.com>
2022-06-20 10:11:20 +0530

    Added Current Date and Time in Log.txt

--- src/FileLog/file-log.cpp
+++ src/FileLog/file-log.cpp
@@ -1,11 +1,10 @@
 #include "file-log.h"
 
-//TODO: add a current date instead of "Logged Time Today"
 void File_Log::FileLog(){
     //happens in file
     fs.open("Log.txt", ios::app);
     horizontal_line();
-    fs << "                Logged Time Today" << endl;
+    current_time();
     horizontal_line();
     fs.close();
 }
@@ -24,4 +23,46 @@ void File_Log::end_program_line(){
     }
     fs << "\n";   
     fs.close();   
+}
+
+void File_Log::current_time(){
+    fs << "Session Opened: ";
+    fs << ltm->tm_mday << "/" << 1 + ltm->tm_mon << "/" << 1900 + ltm->tm_year;
+    fs << " " << show_twelve_hour_time() << ":";
+    fs << show_zero_min() << ":";
+    fs << show_zero_sec();
+    fs << " " << show_AM_PM() << endl;
+}
+
+string File_Log::show_AM_PM(){
+    if(ltm->tm_hour >= 13){
+        return "PM"; 
+    } else {
+        return "AM";
+    }
+}
+
+int File_Log::show_twelve_hour_time(){
+    if(ltm->tm_hour >= 13){
+        return ltm->tm_hour - 12; 
+    } else {
+        return ltm->tm_hour;
+    }
+}
+
+//TODO: find a way to show zero when the time & date is less than 10 like 09, 01
+int File_Log::show_zero_min(){
+    if(ltm->tm_min < 10){
+        return 0 + ltm->tm_min; 
+    } else {
+        return ltm->tm_min;
+    }
+}
+
+int File_Log::show_zero_sec(){
+    if(ltm->tm_sec < 10){
+        return ltm->tm_sec; 
+    } else {
+        return ltm->tm_sec;
+    }
 }
--- src/FileLog/file-log.h
+++ src/FileLog/file-log.h
@@ -4,8 +4,15 @@
 class File_Log {
 public:
     std::ofstream fs;
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
     void FileLog();
     void horizontal_line();
     void end_program_line();
+    void current_time();
+    string show_AM_PM();
+    int show_twelve_hour_time();
+    int show_zero_min();
+    int show_zero_sec();
 };
