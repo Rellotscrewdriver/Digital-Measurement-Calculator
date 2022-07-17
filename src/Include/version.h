3bfb51643fa5d5d218a6491d60c304166d3d1beb
Rellot's Screwdriver <JKanimations71@gmail.com>
2022-07-17 09:58:05 +0530

    Updated the Version Number to 2.1.1

--- src/Include/version.h
+++ src/Include/version.h
@@ -2,7 +2,7 @@
 
 namespace version {
     //Release Date
-    static const char RELEASE_DATE[] = "10/7/2022";
+    static const char RELEASE_DATE[] = "18/7/2022";
 
 	//Software Status
 	static const char STATUS[] =  "Release";
@@ -11,8 +11,8 @@ namespace version {
 	//Semantic Version Type
 	static const long MAJOR_CHANGE  = 2;
 	static const long MINOR_CHANGE  = 1;
-	static const long PATCH  = 0;
+	static const long PATCH  = 1;
 
 	//Miscellaneous Version Types
-	static const char FULLVERSION_STRING[] = "2.1.0";
+	static const char FULLVERSION_STRING[] = "2.1.1";
 }
